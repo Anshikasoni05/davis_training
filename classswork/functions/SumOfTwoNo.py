71b1b2356ae6a1113f6398485314e05637bebf2a
Anshika soni <sonianshi8881@gmail.com>
2026-03-17 11:02:47 +0530

    Program to add two numbers using a function

--- classswork/functions/SumOfTwoNo.py
+++ classswork/functions/SumOfTwoNo.py
@@ -1,6 +1,21 @@
-def addNumbers(x,y):
-    return(x+y)
-a = int(input("enter first number"))
-b = int(input("enter second number"))
-print("sum of",a,"and",b,"is",addNumbers(a,b))
+# Program to add two numbers using a function
+
+# Function definition to add two numbers
+def add_numbers(num1, num2):
+    # Add the two numbers
+    result = num1 + num2
+    
+    # Return the result
+    return result
+
+
+# Taking input from the user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Calling the function and storing the result
+sum_result = add_numbers(a, b)
+
+# Displaying the result
+print("The sum of the two numbers is:", sum_result)
 
