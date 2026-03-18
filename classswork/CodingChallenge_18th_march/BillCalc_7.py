00b224c653d067efdcad90e2362909912134dd66
Anshika soni <sonianshi8881@gmail.com>
2026-03-18 10:15:21 +0530

    Program to calculate final bill after discount using function with user choice

---
+++ classswork/CodingChallenge_18th_march/BillCalc_7.py
@@ -0,0 +1,13 @@
+# Method 8: Ask user if discount should be applied
+#Program to calculate final bill after discount
+def calculate_bill(price, discount, apply_discount):
+    if apply_discount == "yes":
+        return price - (price * discount / 100)
+    else:
+        return price
+
+price = float(input("Enter price: "))
+discount = float(input("Enter discount: "))
+choice = input("Apply discount? (yes/no): ")
+
+print("Final price:", calculate_bill(price, discount, choice))
