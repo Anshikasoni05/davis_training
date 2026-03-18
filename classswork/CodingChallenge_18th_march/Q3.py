a231d12a8b469d82c3625f65c8f3dc47c6a2c10b
Anshika soni <sonianshi8881@gmail.com>
2026-03-18 09:52:11 +0530

    # Program to check whether a person is eligible to vote using ternary operator

---
+++ classswork/CodingChallenge_18th_march/Q3.py
@@ -0,0 +1,9 @@
+# Program to check whether a person is eligible to vote
+# Taking input
+age = int(input("Enter age: "))
+
+# Using one-line condition
+result = "Eligible" if age >= 18 else "Not Eligible"
+
+# Printing result
+print(result)
