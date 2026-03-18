c62c6a5ea74c8be9adc29ca007e810b38a5c5550
Anshika soni <sonianshi8881@gmail.com>
2026-03-18 10:06:47 +0530

    Program to calculate final bill after discount using function with if else statement

---
+++ classswork/CodingChallenge_18th_march/BillCalc_4.py
@@ -0,0 +1,13 @@
+# Method 3: If discount is 0
+#Program to calculate final bill after discount
+
+def calculate_bill(price, discount):
+    if discount == 0:
+        return price   # no change
+    else:
+        return price - (price * discount / 100)
+
+price = float(input("Enter price: "))
+discount = float(input("Enter discount: "))
+
+print("Final price:", calculate_bill(price, discount))
