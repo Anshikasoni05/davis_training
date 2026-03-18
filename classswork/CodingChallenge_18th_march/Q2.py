7ad35167daa8349c9f0bc0f0de39e95d94fdef73
Anshika soni <sonianshi8881@gmail.com>
2026-03-18 09:49:15 +0530

    Program to check whether a person is eligible to vote

--- classswork/CodingChallenge_18th_march/Q2.py
+++ classswork/CodingChallenge_18th_march/Q2.py
@@ -1,9 +1,11 @@
-# Function to calculate final price
-def calculate_bill(price, discount):
-    return price - (price * discount / 100)
+# Program to check whether a person is eligible to vote
 
-# Calling function
-result = calculate_bill(1000, 10)
+# Taking age as input from the user
+age = int(input("Enter your age: "))
 
-# Printing result
-print("Final price:", result)
+# Checking the eligibility condition
+# In India, a person can vote if age is 18 or above
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible to vote")
