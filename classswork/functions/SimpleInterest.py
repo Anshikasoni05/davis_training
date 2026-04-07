c75c915847c074ececec5f49e112e35805f70b74
Anshika soni <sonianshi8881@gmail.com>
2026-04-07 10:53:08 +0530

    calculate SI using function

--- classswork/functions/SimpleInterest.py
+++ classswork/functions/SimpleInterest.py
@@ -1,8 +1,9 @@
+#function for calculating simple interest
 def calculateSI(p,r,t):
     return((p*r*t)/100)
-
+#taking input
 principal = float(input("enter principal"))
 rate = float(input("enter rate "))
 time = float(input("enter time in years"))
-
+#displaying result
 print("Simple Interest on amount",principal,"at rate",rate,"%","in",time,"years is",calculateSI(principal,rate,time))
